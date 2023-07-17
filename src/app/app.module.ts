ec01d3d1468dc9e0f6e22c8f27ecc3186bf51c12
Jeremy dubrulle <jerem-du-75020@hotmail.fr>
2023-07-17 16:58:27 +0200

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MonComposantComponent } from './mon-composant/mon-composant.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonComposantComponent,
+    NavBarComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule
